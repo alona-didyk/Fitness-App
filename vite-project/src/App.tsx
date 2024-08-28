08d5c00cfd625e2cfd12ae81dc5d63a6c9beadcd
Alona Didyk <alionadiduk31@gmail.com>
2024-08-28 19:06:24 +0300

    finished benefits section

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
 import Home from "./scenes/home";
 import Benefits from "./scenes/benefits";
+import OurClasses from "./scenes/ourclasses";
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>
@@ -29,6 +30,7 @@ function App() {
         setSelectedPage={setSelectedPage}/>
 		<Home setSelectedPage={setSelectedPage}/>
 		<Benefits setSelectedPage={setSelectedPage}/>
+		<OurClasses setSelectedPage={setSelectedPage}/>
 	</div>;
 }
 
